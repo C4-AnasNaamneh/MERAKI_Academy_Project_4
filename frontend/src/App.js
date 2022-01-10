8d5a424221e6cba1faf855a1a3a59b22c3bb62a2
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-10 20:24:31 +0200

    frontendnavigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
 import "./App.css";
+import {Routes,Route,Link} from "react-router-dom"
+import Navigation from "./components/Navigation";
+import Register from "./components/Register";
 
-function App() {
+
+const  App = () => {
   return (
+    <>
     <div className="App">
       <h1>Welcome To App</h1>
-    </div>
+
+ <Navigation/> 
+
+<Routes>
+
+<Route path="/register" element = {<Register/>} />
+
+
+
+
+</Routes>
+
+</div>
+
+    </>
   );
 }
 
---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Routes,Route,Link} from "react-router-dom"
+
+
+const Navigation = () =>{
+
+return (
+<>
+
+<Link to="/register">Register</Link>
+
+</>
+
+
+)
+
+
+}
+
+
+export default Navigation;
