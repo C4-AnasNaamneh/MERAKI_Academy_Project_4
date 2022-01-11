2e504396c3857612965e85750fb9ee1aadae75a1
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-11 12:45:45 +0200

    add cart link

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,13 +2,32 @@ import React from "react";
 import {Routes,Route,Link} from "react-router-dom"
 
 
-const Navigation = () =>{
+const Navigation = ({isLoggedIn}) =>{
 
 return (
 <>
+<div className="navigation">
 
-<Link to="/register">Register</Link>
+ {isLoggedIn ?  (
+<>
+<Link to="/products">Products</Link>
 
+<Link to="/newproduct">New Products</Link>
+
+<Link to="/cart">Cart</Link>
+</>
+
+ ): (
+<>
+ <Link to="/register">Register</Link>
+<Link to="/login">Login</Link>
+</>
+)
+
+ }
+
+
+</div>
 </>
 
 
