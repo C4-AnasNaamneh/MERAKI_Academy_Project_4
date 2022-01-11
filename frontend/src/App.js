c97bf8e23d5372476942272860ca987a754852a5
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-11 18:39:27 +0200

    modify app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,55 @@
 import React from "react";
 import "./App.css";
-import {Routes,Route,Link} from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Products from "./components/Products";
-import { useState } from "react";
-const  App = () => {
 
-const [token,setToken] = useState("");
-const [isLoggedIn,setIsLoggedIn] = useState(false) 
+import { useState } from "react";
+import NewProduct from "./components/admin/NewProduct"
+
+import ProductsAdmin from "./components/admin/ProductsAdmin";
+
+const App = () => {
+  const [token, setToken] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
-    <div className="App">
-      <h1>Welcome To App</h1>
+      <div className="App">
+        <h1>Welcome To App</h1>
 
- <Navigation isLoggedIn={isLoggedIn}/> 
+        <Navigation isLoggedIn={isLoggedIn} />
 
-<Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
 
-<Route path="/register" element = {<Register/>} />
+          <Route
+            path="/login"
+            element={
+              <Login setToken={setToken} setIsLoggedIn={setIsLoggedIn} />
+            }
+          />
+
+          <Route path="/products" element={<Products token={token} />} />
+
+          <Route path="/newproduct" element={<NewProduct token={token} />} />
 
 
-<Route path="/login" element = {<Login setToken={setToken} setIsLoggedIn={setIsLoggedIn}/>} />
+
+         <Route path="/adminproducts" element={<ProductsAdmin token={token} />} /> 
 
 
-<Route path="/products" element={<Products/>}/>
 
-</Routes>
+          {/* <Route path="/cart" element={<P />} /> */}
 
-</div>
 
+
+        </Routes>
+      </div>
     </>
   );
-}
+};
 
 export default App;
