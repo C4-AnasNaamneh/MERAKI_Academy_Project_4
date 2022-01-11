52e80d81c12974ceb8851b8264330bac3ff9145d
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-11 12:26:38 +0200

    Created product component

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import axios from "axios";
 
-const Products = () =>{
+const Products = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [img, setImg] = useState("");
 
+  const [products, setProducts] = useState([]);
+  const [message, setMessage] = useState("");
 
+  const getAllProducts = ({ token }) => {
+    axios
+      .get("http://localhost:5000/products", {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then((result) => {
+        console.log(result);
+         setProducts(result.data);
+      })
 
-    
-    return (
+      //لسا بشوف شو في بالداتا
+      .catch((err) => {
+        setMessage(err.response.data.message);
+      });
+  };
+
+  return (
+    <>
+      <p>Products</p>
+      <button onClick={getAllProducts}>GetAllProducts</button>
+
+      {products && products.map((element)=>(
 <>
-<p>Products</p>
-
-
+<img src={element.img}/>
+<p>{element.title}</p>
+<p>{element.description}</p>
+<p>{element.price}</p>
 </>
+  ))
+      
+      
+      
+      
+      }
+    </>
+  );
+};
 
-
-    )
-
-}
-
-export default Products
+export default Products;
