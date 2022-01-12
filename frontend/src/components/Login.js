9b8741e6d42f02c6ec0fb8b663a55674dcf3cfc6
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-12 13:13:42 +0200

    store role from backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,12 +3,13 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Login = ({ setToken, setIsLoggedIn }) => {
+const Login = ({ setToken, setIsLoggedIn,setRole }) => {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
 
   const [password, setPassword] = useState("");
+
 
   const userLogin = {
     email: email,
@@ -41,6 +42,10 @@ const Login = ({ setToken, setIsLoggedIn }) => {
             axios
               .post("http://localhost:5000/login", userLogin)
               .then((result) => {
+                // console.log(result.data.role);
+              setRole(result.data.role)
+                   
+                
                 setToken(result.data.token);
                 localStorage.setItem("token", result.data.token);
                 setIsLoggedIn(true);
