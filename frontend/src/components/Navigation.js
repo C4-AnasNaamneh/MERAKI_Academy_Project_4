0e3e464b16e4e88ec0d79973d38bde02d683e2d1
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-12 12:52:49 +0200

    check if token exists

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,52 +1,37 @@
 import React from "react";
-import {Routes,Route,Link} from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom";
 
+const Navigation = ({ isLoggedIn, setIsLoggedIn, setToken }) => {
+  const token = localStorage.getItem("token");
+  const logout = () => {
+    setToken("");
+    setIsLoggedIn(false);
+  };
 
-const Navigation = ({isLoggedIn,setIsLoggedIn,setToken}) =>{
+  return (
+    <>
+      <div className="navigation">
+        {token ? (
+          <>
+            <Link to="/products">Products</Link>
 
+            <Link to="/newproduct">New Products</Link>
 
-const logout = ()=>{
-    setToken("")
-    setIsLoggedIn(false)
+            <Link to="/cart">Cart</Link>
 
-}
-
-
-
-return (
-<>
-<div className="navigation">
-
- {isLoggedIn ?  (
-<>
-<Link to="/products">Products</Link>
-
-<Link to="/newproduct">New Products</Link>
-
-<Link to="/cart">Cart</Link>
-
-
-<Link to="/login"><button onClick={logout}>Log Out</button></Link>
-</>
-
- ): (
-<>
- <Link to="/register">Register</Link>
-<Link to="/login">Login</Link>
-</>
-)
-
- }
-
-
-</div>
-</>
-
-
-)
-
-
-}
-
+            <Link to="/login">
+              <button onClick={logout}>Log Out</button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Navigation;
