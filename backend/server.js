9b38887e036377d98bbda937ac3cdb5c7682deae
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-16 12:29:49 +0200

    require search router

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,16 @@ app.use(cors());
  const usersRouter = require("./routes/users");
  const loginRouter = require("./routes/login");
 const productsRouter = require("./routes/products")
+const ordersRouter = require("./routes/orders")
+const searchRouter = require("./routes/search")
 
 app.use(express.json());
 
  app.use("/",usersRouter);
  app.use("/",loginRouter);
 app.use("/",productsRouter);
+app.use("/",ordersRouter);
+app.use("/",searchRouter);
 
 const PORT = 5000;
 
