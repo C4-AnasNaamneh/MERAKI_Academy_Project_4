9e9f9e9ded84ccc1187a2189b9e7f3802e472996
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 10:57:00 +0200

    add authorization to route

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,22 +1,12 @@
 const express = require("express");
 
-const {createNewOrder,getAllOrders} = require("../controllers/orders")
-
+const { createNewOrder, getAllOrders } = require("../controllers/orders");
+const { authentication } = require("../middleware/authentication");
 
 const ordersRouter = express.Router();
 
+ordersRouter.post("/orders/",authentication, createNewOrder);
 
+ordersRouter.get("/orders",authentication, getAllOrders);
 
-ordersRouter.post("/orders",createNewOrder)
-
-ordersRouter.get("/orders",getAllOrders)
-
-
-
-
-
-module.exports = ordersRouter
-
-
-
-
+module.exports = ordersRouter;
