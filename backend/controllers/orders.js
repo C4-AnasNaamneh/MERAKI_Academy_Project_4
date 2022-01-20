25a6cd5098e1eca740ee42dcc104946d760aaf15
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 10:54:53 +0200

    modify orders backend

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,7 +1,11 @@
 const orderModel = require("../database/models/ordersSchema");
 
 const createNewOrder = (req, res) => {
-  const { product_id, user_id, totalPrice } = req.body;
+  //const product_id = req.params.id;
+  const { totalPrice, product_id } = req.body;
+  // const product_idsoso = req.body
+
+  const user_id = req.token.userId;
 
   const order = new orderModel({
     product_id,
@@ -21,25 +25,25 @@ const createNewOrder = (req, res) => {
 };
 
 const getAllOrders = (req, res) => {
-
-    orderModel
+  orderModel
     .find({})
-    .then((result)=>{
+    .populate("product_id")
+    .populate("user_id")
 
-    res.status(200).json(result)
-
-    }).catch((err)=>{
-        res.status(500).json({
-            success: false,
-            message: "server error",
-          });
+    .then((result) => {
+      res.status(200).json(result);
+      console.log();
+      //totalPrice = [result.data.product_id]
     })
-
-
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+      });
+    });
 };
 
-module.exports = { 
-    createNewOrder,
-     getAllOrders 
-    
-    };
+module.exports = {
+  createNewOrder,
+  getAllOrders,
+};
