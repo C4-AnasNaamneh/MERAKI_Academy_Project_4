8dc99a3870be0474745662659c2ea74b8ab00e60
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 10:55:17 +0200

    modify order schema

--- backend/database/models/ordersSchema.js
+++ backend/database/models/ordersSchema.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const ordersSchema = new mongoose.Schema({
-    
-    product_id: {type:mongoose.Types.ObjectId, ref:"Products"},
-    user_id: {type:mongoose.Types.ObjectId, ref:"User"},
-    totalPrice: {type:Number}
+  product_id: [{ type: mongoose.Types.ObjectId, ref: "Products" }],
+  user_id: { type: mongoose.Types.ObjectId, ref: "User" },
+  totalPrice: { type: Number },
+});
 
-})
-
-module.exports = mongoose.model("Orders",ordersSchema)
+module.exports = mongoose.model("Orders", ordersSchema);
