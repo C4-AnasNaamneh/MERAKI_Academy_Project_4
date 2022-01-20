a96c6c45710488aca16cb3bf436fa95040496af1
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 22:27:47 +0200

    css

--- frontend/src/components/Home..js
+++ frontend/src/components/Home..js
@@ -6,18 +6,50 @@ const Home = () => {
       <div className="homeImg">
         <img src="https://www.nicepng.com/png/detail/255-2559542_ecommorce-e-commerce-background-png.png" />
 
-       
+        <div className="about">
+          {/* <p>Who We Are</p> */}
+          <p>
+            <em>Who We Are </em>
+          </p>
 
-
-<div  className="about">
-<p>Who We Are</p>
-<p className="aboutParagraph">About E-Buy:
-Founded in 2022, E-Buy is a full-service, ecommerce and interactive agency headquartered in Amman City. Best known for our unique process methodology, </p>
-
-</div>
-
-
+          <p className="aboutParagraph">
+            About E-Buy: Founded in 2022, E-Buy is a full-service, ecommerce and
+            interactive agency headquartered in Amman City.
+          </p>
+        </div>
       </div>
+      <footer className="footer">
+        <p >Author: Anas Naamneh</p>
+        <p >
+          <a href="mailto:naamnih.anas@gmail.com">naamnih.anas@gmail.com</a>
+        </p>
+
+      
+
+
+        <p>
+          <a href="/">Delivery</a>
+        </p>
+
+        <p>
+          <a href="/">Terms and Conditions</a>
+        </p>
+
+        <p>
+          <a href="/">Secure Payment</a>
+  
+        </p>
+        <br/>
+      
+
+
+      </footer>
+      <div className="copyRights">
+          
+          <h1 className="copyRights"> Copyright &#169; 2022 All Rights Reserved by Anas Naamneh</h1>
+          </div>
+
+
     </>
   );
 };
