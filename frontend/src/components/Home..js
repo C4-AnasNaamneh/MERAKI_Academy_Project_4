7a5f26f48d34f2043f95c8ca5d1d4569323b0fd8
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 15:20:09 +0200

    add bakground image

---
+++ frontend/src/components/Home..js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="homeImg">
+        <img src="https://www.nicepng.com/png/detail/255-2559542_ecommorce-e-commerce-background-png.png" />
+
+       
+
+
+<div  className="about">
+<p>Who We Are</p>
+<p className="aboutParagraph">About E-Buy:
+Founded in 2022, E-Buy is a full-service, ecommerce and interactive agency headquartered in Amman City. Best known for our unique process methodology, </p>
+
+</div>
+
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
