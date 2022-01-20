0b9c7432695726f4861d653160431f52b711254d
AnasNaamneh <naamnih.anas@gmail.com>
2022-01-20 11:01:31 +0200

    modify search bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Search from "./Search";
 
 const Navigation = ({ isLoggedIn, setIsLoggedIn, setToken, isAdmin }) => {
   const token = localStorage.getItem("token");
@@ -7,32 +8,82 @@ const Navigation = ({ isLoggedIn, setIsLoggedIn, setToken, isAdmin }) => {
     setToken("");
     setIsLoggedIn(false);
     localStorage.removeItem("token");
-
   };
 
   return (
     <>
       <div className="navigation">
+        <div className="websiteName">
+          {" "}
+          <h1>E<em>-Buy</em></h1>{" "}
+        </div>
+
+        {<Search />}
+
+
         {token ? (
           <>
-          
-           { isAdmin? <></> : <Link to="/products">Products</Link>}
+            {isAdmin ? (
+              <></>
+            ) : (
+              <Link to="/products" className="productsLink">
+                Products
+              </Link>
+            )}
 
-            {isAdmin ? <Link to="/adminproducts">Admin Products</Link> : <></>}
+            {isAdmin ? (
+              <Link to="/adminproducts" className="adminProductsLink">
+                Admin Products
+              </Link>
+            ) : (
+              <></>
+            )}
 
-            {isAdmin ? <Link to="/newproduct">New Products</Link> : <></>}
+            {isAdmin ? (
+              <Link to="/newproduct" className="newProductsLink">
+                New Products
+              </Link>
+            ) : (
+              <></>
+            )}
 
-            {isAdmin ? <></>: <Link to="/cart">Cart</Link>}
+            {isAdmin ? (
+              <></>
+            ) : (
+              <Link to="/cart" className="cartLink">
+                <i class="fas fa-shopping-cart"></i>
+                Cart
+              </Link>
+            )}
 
+            {isAdmin ? (
+              <Link to="/orders" className="ordersLink">
+                Orders
+              </Link>
+            ) : (
+              <></>
+            )}
 
             <Link to="/login">
-              <button onClick={logout}>Log Out</button>
+              <button onClick={logout} className="logoutButton">
+                <i class="fas fa-window-close"></i>
+              </button>
             </Link>
           </>
         ) : (
           <>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
+            <div className="registerNav">
+              <Link to="/register" className="registerLink">
+                Register
+              </Link>
+              </div>
+<div>
+
+<Link to="/login" className="loginLink">
+                Login
+              </Link>
+</div>
+           
           </>
         )}
       </div>
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,72 @@
-import react from "react";
+import React from "react";
 import { useState } from "react/cjs/react.development";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import View from "./View";
 
-const Search = () => {
-    const [search,setSearch] = useState([])
+const Search = ({ token, setView }) => {
+  // const [img, setImg] = useState("");
+
+  const [title, setTitle] = useState("");
+
+  // const [description, setDescription] = useState("");
+  // const [price, setPrice] = useState("");
+
+  const [search, setSearch] = useState([]);
+
+  const navigate = useNavigate();
+
+  const filteredProducts = () => {
+    axios
+      .post(
+        "http://localhost:5000/search",
+        { title: title },
+        {
+          headers: {
+            Authorization: `Bearer  ${token}`,
+          },
+        }
+      )
+      .then((result) => {
+        setSearch(result.data);
+        setView(result.data);
+        
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+
+    navigate("/view");
+  // navigate("/search")
+  };
+  //console.log(search);
   return (
     <>
-      <input type="text" placeholder="Search..." onChange={(e)=>{
-      setSearch(e.target.value)
-      }} className="search"></input>
 
-      <button onClick className="searchButton">Search</button>
+{search &&
+          search.map((element) => (
+            <>
+              <img src={element.img} className="productsImg" />
+              <p>{element.title}</p>
+              <p>{element.description}</p>
+              <p>{element.price}</p>
+            </>
+          ))}
+
+<div className="searchDiv">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        className="search"
+      ></input>
+
+      <button onClick={filteredProducts} className="searchButton">
+        Search
+      </button>
+      </div>
     </>
   );
 };
